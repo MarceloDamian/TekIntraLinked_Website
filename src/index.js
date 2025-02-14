d8c76fca41339a4de9565306c60405ca17ff4fe7
Erick Cabrera <60354250+MarceloDamian@users.noreply.github.com>
2025-02-14 05:29:17 -0500

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {useState} from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import BottomFooter from './components/BottomFooter';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
